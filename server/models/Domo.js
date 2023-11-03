1496c1509b536a5410f2743135c8b814a45b4877
Judy Derrick <jgd6426@rit.edu>
2023-11-02 23:33:03 -0400

    DomoMakerB up to #17
    makeDomo not working?

---
+++ server/models/Domo.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+const models = require('../models');
+
+const { Domo } = models;
+
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  age: doc.age,
+});
+
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'Both name and age are required!' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.json({ redirect: '/maker' });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists!' });
+    }
+
+    return res.status(500).json({ error: 'An error occured making domo!' });
+  }
+};
+
+module.exports = {
+//   makerPage,
+  makeDomo,
+};

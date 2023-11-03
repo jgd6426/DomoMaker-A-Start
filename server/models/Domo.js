f8cfc66036d4d1cefdf5028626c794b1cd3e3781
Judy Derrick <jgd6426@rit.edu>
2023-11-03 12:22:20 -0400

    fixing issue
    theres two domo.js files (one in controllers and one in models), I just had them mixed up

--- server/models/Domo.js
+++ server/models/Domo.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const _ = require('underscore');
-const models = require('../models');
-
-const { Domo } = models;
 
 const setName = (name) => _.escape(name).trim();
 
@@ -34,32 +31,5 @@ DomoSchema.statics.toAPI = (doc) => ({
   age: doc.age,
 });
 
-const makeDomo = async (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'Both name and age are required!' });
-  }
-
-  const domoData = {
-    name: req.body.name,
-    age: req.body.age,
-    owner: req.session.account._id,
-  };
-
-  try {
-    const newDomo = new Domo(domoData);
-    await newDomo.save();
-    return res.json({ redirect: '/maker' });
-  } catch (err) {
-    console.log(err);
-    if (err.code === 11000) {
-      return res.status(400).json({ error: 'Domo already exists!' });
-    }
-
-    return res.status(500).json({ error: 'An error occured making domo!' });
-  }
-};
-
-module.exports = {
-//   makerPage,
-  makeDomo,
-};
+const DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports = DomoModel;
